c8304286b3835aa153f70a16923df248e91a191b
Bongani Masango <bkmasango26@gmail.com>
2025-03-27 10:32:01 +0200

    seeded the database with image data for classifieds

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { seedTaxonomy } from './taxonomy.seed';
 import { seedClassifieds } from './classifieds.seed';
+import { seedImages } from './images.seed';
 
 const prisma = new PrismaClient();
 
@@ -8,8 +9,8 @@ async function main() {
   //   await prisma.$executeRaw`TRUNCATE TABLE "makes" RESTART IDENTITY CASCADE`;
   // await prisma.$executeRaw`TRUNCATE TABLE "classifieds" RESTART IDENTITY CASCADE`;
   //   await seedTaxonomy(prisma);
-  await seedClassifieds(prisma);
-  // await seedImages(prisma);
+  //   await seedClassifieds(prisma);
+  await seedImages(prisma);
   // await seedAdmin(prisma);
   // await seedCustomers(prisma);
 }
